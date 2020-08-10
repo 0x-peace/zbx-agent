62267c189cb9a9dba8513df9de50340b545c0e62
Ivan Kondratyev <peacetoall123@gmail.com>
2020-08-10 21:51:52 +0600

    update to 5.0, add tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,6 +5,8 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
+package_version = "5.0.2"
+
 
 def test_hosts_file(host):
     f = host.file('/etc/hosts')
@@ -12,3 +14,9 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_zabbix_agent_install(host):
+    zabbix_agent = host.package('zabbix-agent')
+    assert zabbix_agent.is_installed
+
